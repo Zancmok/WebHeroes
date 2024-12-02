0f3fd83bbbb72a5efdbcc74386eae827f53eaf59
Zancmok <nejc.germovsek@gmail.com>
2024-12-02 08:42:16 +0100

    Fully configured discord API settings.

--- src/WebHeroes/config.py
+++ src/WebHeroes/config.py
@@ -3,3 +3,4 @@ PORT: int = 5000
 HOST: str = "0.0.0.0"
 STATIC_PATH: str = "../static"
 TEMPLATES_PATH: str = "../templates"
+DISCORD_OAUTH_URL: str = r"https://discord.com/oauth2/authorize?client_id=1313095758970486799&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A5000%2Foauth&scope=identify"
