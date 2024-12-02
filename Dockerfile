c2989312a3325376bc036d907afc7cf0dce38835
Zancmok <nejc.germovsek@gmail.com>
2024-12-02 18:26:07 +0100

    Started implementing discord oauth.

--- Dockerfile
+++ Dockerfile
@@ -10,6 +10,8 @@ COPY ./src /app/src
 
 COPY requirements.txt /app/
 
+COPY .env /app/
+
 RUN pip install --no-cache-dir -r /app/requirements.txt
 
 EXPOSE 5000
--- src/WebHeroes/WebHeroes.py
+++ src/WebHeroes/WebHeroes.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from zenora import APIClient
+from zenora.models.oauth import OauthResponse
 import WebHeroes.config as config
 
 
@@ -14,11 +16,17 @@ class WebHeroes:
         static_folder=config.STATIC_PATH
     )
 
+    discord_client: APIClient = APIClient(
+        token=config.DISCORD_BOT_TOKEN,
+        client_secret=config.DISCORD_CLIENT_SECRET
+    )
+
     @staticmethod
     def run() -> None:
         """
         Runs the static WebHeroes.app Flask App.
         """
+
         WebHeroes.app.run(
             host=config.HOST,
             port=config.PORT,
@@ -34,7 +42,22 @@ class WebHeroes:
     @app.route("/modding-documentation/")
     def modding_documentation() -> str:
         return render_template("modding-documentation.html")
-        
+
+    @staticmethod
+    @app.route("/oauth/")
+    def oauth() -> str:
+        oauth_code: str = request.args.get('code', '_')
+
+        if oauth_code == '_':
+            return render_template("index.html")
+
+        access_token: OauthResponse = WebHeroes.discord_client.oauth.get_access_token(
+            code=oauth_code,
+            redirect_uri=config.DISCORD_OAUTH_URL
+        )
+
+        return ""
+
     @staticmethod
     @app.route("/online-lobbies/")
     def online_lobbies() -> str:
