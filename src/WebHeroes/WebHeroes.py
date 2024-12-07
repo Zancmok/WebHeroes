241d625257df25c71147c9a4d0b43efe9ab5c54e
Zancmok <nejc.germovsek@gmail.com>
2024-12-07 18:44:37 +0100

    Fully implemented discord oauth.

--- src/WebHeroes/WebHeroes.py
+++ src/WebHeroes/WebHeroes.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect
+from werkzeug import Response
 from zenora import APIClient
 from zenora.models.oauth import OauthResponse
+from zenora.models.user import OwnUser
 import WebHeroes.config as config
 
 
@@ -27,6 +29,8 @@ class WebHeroes:
         Runs the static WebHeroes.app Flask App.
         """
 
+        WebHeroes.app.config["SECRET_KEY"] = config.FLASK_SECRET_KEY
+
         WebHeroes.app.run(
             host=config.HOST,
             port=config.PORT,
@@ -45,22 +49,35 @@ class WebHeroes:
 
     @staticmethod
     @app.route("/oauth/")
-    def oauth() -> str:
+    def oauth() -> Response:
         oauth_code: str = request.args.get('code', '_')
 
         if oauth_code == '_':
-            return render_template("index.html")
+            return redirect("/")
 
-        access_token: OauthResponse = WebHeroes.discord_client.oauth.get_access_token(
+        oauth_response: OauthResponse = WebHeroes.discord_client.oauth.get_access_token(
             code=oauth_code,
-            redirect_uri=config.DISCORD_OAUTH_URL
+            redirect_uri=config.REDIRECT_URI
         )
 
-        return ""
+        access_token: str = oauth_response.access_token
+
+        session['access_token'] = access_token
+
+        """bearer_client: APIClient = APIClient(access_token, bearer=True)
+
+        current_user: OwnUser = bearer_client.users.get_current_user()"""
+
+        return redirect("/online-lobbies/")
 
     @staticmethod
     @app.route("/online-lobbies/")
-    def online_lobbies() -> str:
-        print("Darn")
-        
-        return ""
+    def online_lobbies() -> str | Response:
+        if not session.get('access_token', ''):
+            return redirect(config.DISCORD_OAUTH_URL)
+
+        bearer_client: APIClient = APIClient(session['access_token'], bearer=True)
+
+        current_user: OwnUser = bearer_client.users.get_current_user()
+
+        return f"<h1>{current_user.username}</h1>"
--- src/WebHeroes/config.py
+++ src/WebHeroes/config.py
@@ -1,15 +1,17 @@
+import os
+import dotenv
+import urllib.parse
+
+dotenv.load_dotenv()
+
 DEBUG: bool = True
 PORT: int = 5000
 HOST: str = "0.0.0.0"
 STATIC_PATH: str = "../static"
 TEMPLATES_PATH: str = "../templates"
-DISCORD_OAUTH_URL: str = r"https://discord.com/oauth2/authorize?client_id=1313095758970486799&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A5000%2Foauth&scope=identify"
-
-import os
-import dotenv
-
-dotenv.load_dotenv()
-
+REDIRECT_URI: str = "http://localhost:5000/oauth"
+DISCORD_OAUTH_URL: str = f"https://discord.com/oauth2/authorize?client_id=1313095758970486799&response_type=code&redirect_uri={urllib.parse.quote(REDIRECT_URI)}&scope=identify"
 DISCORD_CLIENT_ID: str = os.getenv("DISCORD_CLIENT_ID")
 DISCORD_CLIENT_SECRET: str = os.getenv("DISCORD_CLIENT_SECRET")
 DISCORD_BOT_TOKEN: str = os.getenv("DISCORD_BOT_TOKEN")
+FLASK_SECRET_KEY: str = os.getenv("FLASK_SECRET_KEY")
