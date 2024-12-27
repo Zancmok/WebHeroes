6f277f1c2c0fe104aef2d442a3b07424efe42c64
Zancmok <nejc.germovsek@gmail.com>
2024-12-27 23:27:44 +0100

    Improved the Database connection.

--- src/WebHeroes/DatabaseBridge.py
+++ src/WebHeroes/DatabaseBridge.py
@@ -1,12 +1,27 @@
 from ZLib.StaticClass import StaticClass
+from ZLib.Console import Console
 import mysql.connector
+from mysql.connector import MySQLConnection
+from mysql.connector.errors import DatabaseError
 import WebHeroes.config as config
+import time
 
 
 class DatabaseBridge(StaticClass):
-    database: mysql.connector.MySQLConnection = mysql.connector.connect(
-        host=config.MYSQL_HOST,
-        user=config.MYSQL_USER,
-        password=config.MYSQL_PASSWORD,
-        database=config.MYSQL_DATABASE
-    )
+    database: MySQLConnection
+
+    while True:
+        Console.print("Connecting to DB!")
+
+        try:
+            database = mysql.connector.connect(
+                host=config.MYSQL_HOST,
+                user=config.MYSQL_USER,
+                password=config.MYSQL_PASSWORD,
+                database=config.MYSQL_DATABASE
+            )
+
+            break
+        except DatabaseError:
+            Console.warn(f"Couldn't connect to DB, retrying in {config.DATABASE_RECONNECTION_TIMEOUT}s!")
+            time.sleep(config.DATABASE_RECONNECTION_TIMEOUT)
