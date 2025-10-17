6006ec77e7e026298da07135447153ff0fd78931
s1lenk <silenkm9@gmail.com>
2025-10-17 15:48:09 +0200

    magic anchor shit doesn't worky work, so now im sad

--- src/WebHeroesApp/scripts/LoginRegisterMenu.cs
+++ src/WebHeroesApp/scripts/LoginRegisterMenu.cs
@@ -11,14 +11,14 @@ public partial class LoginRegisterMenu : Node2D
 		GD.Print("MainMenu _ready() called!");
 		utilityClass = new UtilityClass();
 
-		Button buttonLoginPage = utilityClass.CreateButton(this, "MainMenuButtonPlay", new Vector2(10, 10), new Vector2(100,100));
-		buttonLoginPage.Pressed += ButtonPressed;
-
-		Button buttonRegisterPage = utilityClass.CreateButton(this, "MainMenuButtonSettings", new Vector2(100, 100), new Vector2(10, 10));
-		buttonRegisterPage.Pressed += ButtonPressed;
-
 		Container testContainer = utilityClass.CreateContainer(this, Control.LayoutPreset.Center);
 		CenterContainer testCenterContainer = utilityClass.CreateCenterContainer(testContainer, false, Control.LayoutPreset.Center);
+		
+		Button buttonLoginPage = utilityClass.CreateButton(testCenterContainer, "MainMenuButtonPlay", new Vector2(10, 10), new Vector2(100,100));
+		buttonLoginPage.Pressed += ButtonPressed;
+
+		Button buttonRegisterPage = utilityClass.CreateButton(testCenterContainer, "MainMenuButtonSettings", new Vector2(100, 100), new Vector2(10, 10));
+		buttonRegisterPage.Pressed += ButtonPressed;
 	}
 
 	private void ButtonPressed()
