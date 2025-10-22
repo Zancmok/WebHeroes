0436e3ed5f389067a176cb83db2725f03daded90
s1lenk <166941235+s1lenk@users.noreply.github.com>
2025-10-22 05:41:07 +0000

    spacebar

--- src/WebHeroesApp/scripts/LoginRegisterMenu.cs
+++ src/WebHeroesApp/scripts/LoginRegisterMenu.cs
@@ -6,7 +6,7 @@ using System.Xml.Serialization;
 public partial class LoginRegisterMenu : Node2D
 {
 	private UtilityClass utilityClass;
-	
+
 	public override void _Ready()
 	{
 		GD.Print("MainMenu _ready() called!");
@@ -15,41 +15,41 @@ public partial class LoginRegisterMenu : Node2D
 		//HttpRequest httpRequest = GetNode<HttpRequest>("HTTPRequest"); // add node in editor
 		//httpRequest.RequestCompleted += OnRequestCompleted;
 		//httpRequest.Request("https://jsonplaceholder.typicode.com/posts/1");
-		
-		
-		
+
+
+
 		Control testControl1 = new Control();
 		AddChild(testControl1);
 		testControl1.SetAnchorsPreset(Control.LayoutPreset.FullRect);
 		testControl1.Size = GetViewportRect().Size;
 		testControl1.Position = Vector2.Zero;
-		
+
 		CallDeferred(MethodName.DebugSizes, testControl1);
 		// dynamic elements
 		CenterContainer testCenterContainer1 = utilityClass.CreateCenterContainer(testControl1);
-		
+
 		VBoxContainer vbox = new VBoxContainer();
 		vbox.AddThemeConstantOverride("separation", 20);
 		testCenterContainer1.AddChild(vbox);
-		
-		Button buttonLoginPage = utilityClass.CreateButton(vbox, "MainMenuButtonPlay", new Vector2(200,50));
+
+		Button buttonLoginPage = utilityClass.CreateButton(vbox, "MainMenuButtonPlay", new Vector2(200, 50));
 		buttonLoginPage.Pressed += () => ButtonPressed("success");
 
-		Button buttonRegisterPage = utilityClass.CreateButton(vbox, "MainMenuButtonSettings", new Vector2(200,50));
+		Button buttonRegisterPage = utilityClass.CreateButton(vbox, "MainMenuButtonSettings", new Vector2(200, 50));
 		buttonRegisterPage.Pressed += () => ButtonPressed("success");
-		
-		
+
+
 		// static elements
 		//CenterContainer loginRegisterPageCenterContainer = GetNode<CenterContainer>("LoginRegisterPageCenterContainer");
 		//loginRegisterPageCenterContainer.SetAnchorsPreset(Control.LayoutPreset.FullRect);
-		
+
 		//HBoxContainer loginRegisterButtonContainer = GetNode<HBoxContainer>("LoginRegisterPageContainer/LoginRegisterButtonContainer");
 		//loginRegisterButtonContainer.SetAnchorsPreset(Control.LayoutPreset.TopWide);
-		
+
 		//Button loginButton = GetNode<Button>("LoginRegisterButtonContainer/LoginButton");
 		//loginButton.Text = "Login";
 		//loginButton.Pressed += () => ButtonPressed("Login");
-		
+
 		//Button registerButton = GetNode<Button>("LoginRegisterButtonContainer/RegisterButton");
 		//registerButton.Text = "Register";
 		//registerButton.Pressed += () => ButtonPressed("Register");
@@ -63,7 +63,7 @@ public partial class LoginRegisterMenu : Node2D
 	{
 		throw new NotImplementedException();
 	}
-	
+
 	private void DebugSizes(Control control)
 	{
 		GD.Print($"testControl1 Size: {control.Size}");
