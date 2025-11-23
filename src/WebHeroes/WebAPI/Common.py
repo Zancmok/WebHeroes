078403919fb2ac1be4877c08a1c5d7693cf035ad
Zancmok <nejc.germovsek@gmail.com>
2025-11-23 17:27:01 +0100

    Figure out that 1 thing.

--- src/WebHeroes/WebAPI/Common.py
+++ src/WebHeroes/WebAPI/Common.py
@@ -1,6 +1,10 @@
+from typing import Optional
 import WebHeroes.config as config
 from ZancmokLib.StaticClass import StaticClass
 from ZancmokLib.SocketBlueprint import SocketBlueprint
+from WebHeroes.UserManagement.SessionManager import SessionManager
+from WebHeroes.LobbyManagement.Errors.AlreadyInLobbyError import AlreadyInLobbyError
+from WebHeroes.LobbyManagement.LobbyManager import LobbyManager
 from flask import Blueprint
 
 
@@ -18,10 +22,24 @@ class Common(StaticClass):
 
     @staticmethod
     @socket_blueprint.on("connect")
-    def on_connect() -> None:
-        print("Client connected (:", flush=True)
+    def on_connect(auth: Optional[dict[str, str]]) -> None:
+        user_id: int
+        if not (user_id := SessionManager.get_user_id((auth.get("token") if isinstance(auth, dict) else None))):
+            raise ConnectionRefusedError("unauthorized")
+
+        try:
+            LobbyManager.online_lobby.join_member(user_id)
+        except AlreadyInLobbyError:
+            raise ConnectionRefusedError("User already connected!")
+
+        print(f"Client connected!", flush=True)
 
     @staticmethod
     @socket_blueprint.on("disconnect")
-    def on_disconnect() -> None:
-        print("Client disconnected ):", flush=True)
+    def on_disconnect(reason: str) -> None:
+        user_id: Optional[int] = SessionManager.get_user_id()
+
+        if user_id:
+            LobbyManager.online_lobby.leave_member(user_id)
+
+        print(f"Client disconnected: {reason}", flush=True)
--- src/WebHeroes/WebHeroes/LobbyManagement/Lobby.py
+++ src/WebHeroes/WebHeroes/LobbyManagement/Lobby.py
@@ -1,10 +1,16 @@
+from WebHeroes.LobbyManagement.Errors.AlreadyInLobbyError import AlreadyInLobbyError
+
+
 class Lobby:
     def __init__(self, name: str) -> None:
         self.name: str = name
-        self.member_ids: list[int] = []
+        self.member_ids: set[int] = set()
 
     def join_member(self, member_id: int) -> None:
-        self.member_ids.append(member_id)
+        if member_id in self.member_ids:
+            raise AlreadyInLobbyError
+
+        self.member_ids.add(member_id)
 
     def leave_member(self, member_id: int) -> None:
         self.member_ids.remove(member_id)
