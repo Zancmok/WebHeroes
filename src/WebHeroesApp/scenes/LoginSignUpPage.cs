2c4ad11fa27746754e1cc15e251a04219daeb0c2
s1lenk <silenkm9@gmail.com>
2025-12-12 15:53:54 +0100

    only the http requests to go then onto lobbies

--- src/WebHeroesApp/scenes/LoginSignUpPage.cs
+++ src/WebHeroesApp/scenes/LoginSignUpPage.cs
@@ -8,14 +8,7 @@ public partial class LoginSignUpPage : Control
 	
 	public override void _Ready()
 	{
-		Button showPassword = GetNode<Button>("CenterContainer_BasePlate/BoxContainer/VBoxContainer/FormLogin/VBoxContainer/PasswordInput/ShowPassword");
-		LineEdit passwordLine = GetNode<LineEdit>("CenterContainer_BasePlate/BoxContainer/VBoxContainer/FormLogin/VBoxContainer/PasswordInput/PasswordLine");
-		passwordLine.Secret = true;
-		
-		showPassword.Pressed += () =>
-		{
-			passwordLine.Secret = !passwordLine.Secret;
-		};
+
 		
 		
 	}
--- src/WebHeroesApp/scenes/SignUpVisuals.cs
+++ src/WebHeroesApp/scenes/SignUpVisuals.cs
@@ -4,13 +4,12 @@ using SilenkLibrary;
 
 public partial class SignUpVisuals : HBoxContainer
 {
-	private UtilityClass utilityClass;
 	
 	public override void _Ready()
 	{
-		Button showPassword = GetNode<Button>("CenterContainer_BasePlate/BoxContainer/VBoxContainer/FormSignUp/VBoxContainer/HBoxContainer/ShowPassword");
-		LineEdit passwordLine = GetNode<LineEdit>("CenterContainer_BasePlate/BoxContainer/VBoxContainer/FormSignUp/VBoxContainer/HBoxContainer/VBoxContainer/PasswordInput/PasswordLine");
-		LineEdit repeatPasswordLine = GetNode<LineEdit>("CenterContainer_BasePlate/BoxContainer/VBoxContainer/FormSignUp/VBoxContainer/HBoxContainer/VBoxContainer/RepeatPassword/RepeatPasswordLine");
+		Button showPassword = GetNode<Button>("ShowPassword");
+		LineEdit passwordLine = GetNode<LineEdit>("VBoxContainer/PasswordInput/PasswordLine");
+		LineEdit repeatPasswordLine = GetNode<LineEdit>("VBoxContainer/RepeatPassword/RepeatPasswordLine");
 		passwordLine.Secret = true;
 		repeatPasswordLine.Secret = true;
 		
