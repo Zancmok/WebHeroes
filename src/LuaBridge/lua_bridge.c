b2fd09b4383f2ae441afea23c73f0d278654033a
Zancmok <145902998+Zancmok@users.noreply.github.com>
2026-01-06 09:05:25 +0000

    Started working on LuaBridge

--- src/LuaBridge/lua_bridge.c
+++ src/LuaBridge/lua_bridge.c
@@ -3,6 +3,24 @@
 #include <lauxlib.h>
 #include <lualib.h>
 
+int sandboxAmount = 0;
+lua_State *sandboxes = NULL;
+
+void openLuaSandbox(PyObject *name)
+{
+    lua_State *L = lua_open();
+
+    if (!L)
+    {
+        PyErr_SetString(PyExc_RuntimeError, "Failed to create Lua state");
+        return;
+    }
+
+    sandboxAmount++;
+    sandboxes = (lua_State*)realloc(sandboxes, sizeof(lua_State*) * sandboxAmount);
+    sandboxes[sandboxAmount - 1] = L;
+}
+
 static PyMethodDef lua_bridge_module_methods[] = {
     {NULL, NULL, 0, NULL}
 };
