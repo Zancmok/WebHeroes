5f19133efc0c5b091dafe850af9e4b241263aa8b
Zancmok <nejc.germovsek@gmail.com>
2026-01-07 19:37:39 +0100

    Made the compilation scripts for LuaBridge

--- src/LuaBridge/lua_bridge.c
+++ src/LuaBridge/lua_bridge.c
@@ -42,7 +42,7 @@ static PyTypeObject LuaSandbox_Type = {
     0,                        // tp_setattro
     0,                        // tp_as_buffer
     Py_TPFLAGS_DEFAULT,       // tp_flags
-    "LuaSandbox objects",     // tp_doc
+    0,                        // tp_doc
     0,                        // tp_traverse
     0,                        // tp_clear
     0,                        // tp_richcompare
