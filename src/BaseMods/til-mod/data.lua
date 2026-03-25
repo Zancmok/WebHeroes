8e42021425c41f1caa2505859cf0c7b9b9ca855c
Zancmok <nejc.germovsek@gmail.com>
2026-03-25 14:29:17 +0100

    Modernized Tils mod

--- src/BaseMods/til-mod/data.lua
+++ src/BaseMods/til-mod/data.lua
@@ -1,8 +1,7 @@
-local outer_bound = data.raw.field["outer-bound"]
-
+local outer_bound = data:get_prototype("field", "outer-bound")
 outer_bound.display_name = "Jaha Sea"
 outer_bound.sprite = "__til-mod__/graphics/field/sigma-sea.png"
 
-local settings = data.raw["settings"]["settings"]
-
+local settings = data:get_prototype("settings", "settings")
 settings.map_size = 51
+settings.point_requirement = 100
