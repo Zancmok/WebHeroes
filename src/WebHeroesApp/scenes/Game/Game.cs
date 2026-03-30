3afa11f60a4bc9c813afa41756af6a7995923544
s1lenk <silenkm9@gmail.com>
2026-03-30 10:31:55 +0200

    small fixes and additions

--- src/WebHeroesApp/scenes/Game/Game.cs
+++ src/WebHeroesApp/scenes/Game/Game.cs
@@ -31,7 +31,7 @@ public partial class Game : Node2D
 		string token  = gameState.Get("token").AsString();
 		_socketIOGame.Call("connect_to_server", token);
 
-	    _gameUI = GetNode<Node>("GameUI");
+		_gameUI = GetNode<Node>("GameUI");
 		_gameUI.Connect("end_turn_pressed", new Callable(this, nameof(OnEndTurnPressed)));
 		_gameUI.Connect("build_pressed", new Callable(this, nameof(OnBuildPressed)));
 	}
@@ -44,38 +44,38 @@ public partial class Game : Node2D
 
 private void OnGameDataReceived(Variant raw)
 {
-    Dictionary data;
-    if (raw.AsGodotArray() is { Count: > 0 } arr)
-        data = arr[0].AsGodotDictionary();
-    else
-        data = raw.AsGodotDictionary();
+	Dictionary data;
+	if (raw.AsGodotArray() is { Count: > 0 } arr)
+		data = arr[0].AsGodotDictionary();
+	else
+		data = raw.AsGodotDictionary();
 
-    var hexBoard = GetNode<Node2D>("HexBoard");
-    hexBoard.Call("load_game_data", data);
+	var hexBoard = GetNode<Node2D>("HexBoard");
+	hexBoard.Call("load_game_data", data);
 
-    var bounds = hexBoard.Call("get_board_bounds").AsGodotArray();
-    var camera = GetNode<Camera2D>("GameCamera");
-    camera.Call("set_map_bounds", bounds[0].AsVector2(), bounds[1].AsVector2());
-    camera.Position = (bounds[0].AsVector2() + bounds[1].AsVector2()) / 2f;
+	var bounds = hexBoard.Call("get_board_bounds").AsGodotArray();
+	var camera = GetNode<Camera2D>("GameCamera");
+	camera.Call("set_map_bounds", bounds[0].AsVector2(), bounds[1].AsVector2());
+	camera.Position = (bounds[0].AsVector2() + bounds[1].AsVector2()) / 2f;
 
-    _myIndex = data.TryGetValue("my_index", out var mi) ? mi.AsInt32() : -1;
-    _currentIndex = data.TryGetValue("current_user_index", out var ci) ? ci.AsInt32() : 0;
-    _players = data.TryGetValue("players", out var pl) ? pl.AsGodotArray() : new Array();
-    _recipes = data.TryGetValue("prototypes", out var pr) 
-        ? pr.AsGodotArray().Where(p => p.AsGodotDictionary().TryGetValue("object_type", out var ot) 
-            && ot.AsString() == "recipe-s_prototype").ToGodotArray()
-        : new Array();
+	_myIndex = data.TryGetValue("my_index", out var mi) ? mi.AsInt32() : -1;
+	_currentIndex = data.TryGetValue("current_user_index", out var ci) ? ci.AsInt32() : 0;
+	_players = data.TryGetValue("players", out var pl) ? pl.AsGodotArray() : new Array();
+	_recipes = data.TryGetValue("prototypes", out var pr) 
+		? pr.AsGodotArray().Where(p => p.AsGodotDictionary().TryGetValue("object_type", out var ot) 
+			&& ot.AsString() == "recipe-s_prototype").ToGodotArray()
+		: new Array();
 
-    if (_myIndex >= 0 && _myIndex < _players.Count)
-        _myResources = _players[_myIndex].AsGodotDictionary()
-            .TryGetValue("resources", out var res) ? res.AsGodotDictionary() : new Dictionary();
+	if (_myIndex >= 0 && _myIndex < _players.Count)
+		_myResources = _players[_myIndex].AsGodotDictionary()
+			.TryGetValue("resources", out var res) ? res.AsGodotDictionary() : new Dictionary();
 
-    var isMyTurn = _currentIndex == _myIndex;
-    _gameUI.Call("update_players", _players, _currentIndex, _myIndex);
-    _gameUI.Call("update_resources", _myResources);
-    _gameUI.Call("update_recipes", _recipes, _myResources, isMyTurn);
-    _gameUI.Call("update_turn_banner", _players, _currentIndex, _myIndex);
-    _gameUI.Call("set_end_turn_enabled", isMyTurn);
+	var isMyTurn = _currentIndex == _myIndex;
+	_gameUI.Call("update_players", _players, _currentIndex, _myIndex);
+	_gameUI.Call("update_resources", _myResources);
+	_gameUI.Call("update_recipes", _recipes, _myResources, isMyTurn);
+	_gameUI.Call("update_turn_banner", _players, _currentIndex, _myIndex);
+	_gameUI.Call("set_end_turn_enabled", isMyTurn);
 }
 
 	private void OnEndTurnReceived(Variant raw)
